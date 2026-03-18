9224fb9261db0c7d8a1a969a9a64f9639cf78eb8
romanshrestha20 <stha.roman20@gmail.com>
2026-03-18 23:59:17 +0200

    refactor: streamline authentication error handling in middleware

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -3,20 +3,22 @@ import AppError from '../utils/appError.js';
 import { verifyAccessToken } from '../utils/jwt.js';
 
 export const authenticateUser = (req: Request, res: Response, next: NextFunction) => {
-    const authHeader = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-    try {
-        if (!authHeader || !authHeader.startsWith('Bearer ')) {
-            return next(new AppError('Unauthorized: No token provided', 401));
-        }
-        const token = authHeader.split(' ')[1];
-        verifyAccessToken(token);
-        return next();
-
-    } catch (error) {
-        if (error instanceof AppError) {
-            return next(error);
-        }
-        return next(new AppError('Unauthorized', 401));
+  try {
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return next(new AppError('Unauthorized: No token provided', 401));
     }
+
+    const token = authHeader.split(' ')[1];
+
+    const decoded = verifyAccessToken(token);
+
+    //  attach user info to request
+    (req as any).user = decoded;
+
+    next();
+  } catch (error) {
+    return next(new AppError('Unauthorized', 401));
+  }
 };
