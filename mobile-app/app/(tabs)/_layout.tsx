fbc6660b5fad81695d781de22ef1f02af68adabf
romanshrestha20 <stha.roman20@gmail.com>
2026-04-01 00:00:52 +0300

    feat: enhance TabsLayout with screen options and icons for improved navigation

--- mobile-app/app/(tabs)/_layout.tsx
+++ mobile-app/app/(tabs)/_layout.tsx
@@ -1,14 +1,34 @@
 // app/(tabs)/_layout.tsx
 
 import { Tabs } from "expo-router";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { theme } from "@/design-system";
 
 export default function TabsLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="home" options={{ title: "Home" }} />
-      <Tabs.Screen name="requests" options={{ title: "Requests" }} />
-      <Tabs.Screen name="bids" options={{ title: "Bids" }} />
-      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+     <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.primary,
+        tabBarInactiveTintColor: theme.colors.textSecondary,
+        tabBarStyle: { height: 60, paddingBottom: 5 },
+      }}
+    >
+      <Tabs.Screen 
+      name="home" 
+      options={{ 
+        title: "Home",
+        tabBarIcon: ({ color, size }) => (
+          <FontAwesome name="home" size={size} color={color} />
+        ),
+      }} />
+        <Tabs.Screen name="profile" 
+      options={{
+         title: "Profile",
+         tabBarIcon: ({ color, size }) => (
+           <FontAwesome name="user" size={size} color={color} />
+         ),
+       }} />
     </Tabs>
   );
 }
