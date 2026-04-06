385c2bf082d4d14c34968c990ccef61e44696c6c
romanshrestha20 <stha.roman20@gmail.com>
2026-04-06 15:39:54 +0300

    feat: add user component exports for improved modularity and organization

---
+++ mobile-app/src/features/user/components/index.ts
@@ -0,0 +1,7 @@
+export { ProfileHeaderCard } from "./ProfileHeaderCard";
+export { ProfileAvatarPickerModal } from "./ProfileAvatarPickerModal";
+export { ProfileStatsRow } from "./ProfileStatsRow";
+export { ProfileInfoSection } from "./ProfileInfoSection";
+export { ProfileEditForm } from "./ProfileEditForm";
+export { SettingsSectionCard } from "./SettingsSectionCard";
+export { DangerZoneCard } from "./DangerZoneCard";
