312bb5599871f96261c1a8719481ceb3ecbb51c9
romanshrestha20 <stha.roman20@gmail.com>
2026-04-06 16:40:47 +0300

    feat: update layout components to remove header options in Stack navigation

--- mobile-app/app/(tabs)/notifications/_layout.tsx
+++ mobile-app/app/(tabs)/notifications/_layout.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { Stack } from "expo-router";
 
 export default function NotificationsLayout() {
-    return <Stack screenOptions={{ headerShown: false }} />;
+    return <Stack />;
 }
