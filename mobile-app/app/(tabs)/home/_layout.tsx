12f449aafef75571b6c279606cc466a6cdab868e
romanshrestha20 <stha.roman20@gmail.com>
2026-04-09 11:17:07 +0300

    feat: simplify HomeLayout by removing header configuration and unused screen

--- mobile-app/app/(tabs)/home/_layout.tsx
+++ mobile-app/app/(tabs)/home/_layout.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { Stack } from "expo-router";
 
-import { getAppStackHeaderOptions } from "@/config/headerConfig";
-
 export default function HomeLayout() {
   return (
-    <Stack screenOptions={getAppStackHeaderOptions()}>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="requests/new" options={{ title: "Create Request" }} />
       <Stack.Screen name="requests/index" options={{ title: "Browse Requests" }} />
       <Stack.Screen name="requests/[id]" options={{ title: "Request Details" }} />
       <Stack.Screen name="requests/[id]/edit" options={{ title: "Edit Request" }} />
