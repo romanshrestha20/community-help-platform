525ca079844c752ea055725a9b5d7f0b0f5a745f
romanshrestha20 <stha.roman20@gmail.com>
2026-05-01 00:43:43 +0300

    feat: enhance user authentication by adding token version checks

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -1,8 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 import AppError from '../utils/appError.js';
 import { verifyAccessToken } from '../utils/jwt.js';
+import { prisma } from '../lib/prisma.js';
 
-export const authenticateUser = (req: Request, res: Response, next: NextFunction) => {
+export const authenticateUser = async (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
 
   try {
@@ -13,6 +14,18 @@ export const authenticateUser = (req: Request, res: Response, next: NextFunction
     const token = authHeader.split(' ')[1];
 
     const decoded = verifyAccessToken(token);
+    const user = await prisma.userModel.findUnique({
+      where: { id: decoded.userId },
+      select: { tokenVersion: true },
+    });
+
+    if (!user) {
+      return next(new AppError('Unauthorized', 401));
+    }
+
+    if (decoded.tokenVersion !== user.tokenVersion) {
+      return next(new AppError('Session expired. Please login again.', 401));
+    }
 
     //  attach user info to request
     (req as any).user = decoded;
